a2248000d8a9b7230d06152353cb5bb9e7963358
alihoostudio <hello.alihoo@gmail.com>
2023-02-28 13:24:51 +0300

    new

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,9 @@ export default function Home() {
 				<div
 					className={`h-full w-full min-w-[calc(33.3333333%-2rem)] bg-cool-2`}
 				>
-					<div className={`aspect-3/2 bg-cool-4`}>3/2 and more</div>
+					<div className={`aspect-3/2 bg-cool-4`}>
+						3/2 and more sad
+					</div>
 				</div>
 				<div
 					className={`h-full w-full min-w-[calc(33.3333333%-2rem)] bg-cool-2`}
